7dca4fda909d0b027252c6fb5daca2756fa3cf1e
Carmine Moleti <carmine@moleti.it>
2018-06-02 18:59:23 +0200

    Updated fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+set -x PATH $PATH /home/carmine/.scripts
+set -x PATH $PATH /home/linuxbrew/.linuxbrew/bin
+set -x MANPATH $MANPATH /home/linuxbrew/.linuxbrew/share/man
+set -x INFOPATH $INFOPATH /home/linuxbrew/.linuxbrew/share/info
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
